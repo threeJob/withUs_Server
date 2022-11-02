e6295fb5a85ac96ef21d73d4440f4d61fc773952
PARKYUNJU <pyunj6767@gmail.com>
2022-11-02 18:38:05 +0900

    프로젝트 생성

---
+++ WithUs/src/main/java/com/example/WithUs/WithUsApplication.java
@@ -0,0 +1,13 @@
+package com.example.WithUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithUsApplication.class, args);
+	}
+
+}
