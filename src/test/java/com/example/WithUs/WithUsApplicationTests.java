7fce1f9402fc8280b52d03f73afcafc52f2c33e0
Ktaewon <dydalehdxo@naver.com>
2022-11-03 00:26:36 +0900

    First Code Commit

---
+++ src/test/java/com/example/WithUs/WithUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WithUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
